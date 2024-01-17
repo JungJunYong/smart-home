16abc6ac3c5510d51c2a92341be9968ef2185f84
정준용 <jkoues@douzone.com>
2024-01-17 14:27:25 +0900

    진입점 수정

--- index.ts
+++ index.ts
@@ -1,46 +1,32 @@
 import receiveMsg from "./src/kocom/receiveMsg";
 import net from "net";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-const clients: net.Socket[] = [];
+// 연결할 서버 정보
+const serverHost = process.env.SERVER_HOST as string; // 서버 호스트
+const serverPort = Number(process.env.SERVER_PORT); // 서버 포트
 
-const server = net.createServer(function(client){
-    console.log('new Connection',client.remoteAddress,client.localAddress);
-    if(!global.kocom && client.remoteAddress == '::ffff:14.39.64.167'){
-        global.kocom = client;
-    }
-    clients.push(client);
-    client.on('end', function(){
-      console.log('연결 종료!!',client.remoteAddress);
-        const index = clients.indexOf(client);
-        if (index !== -1) {
-            clients.splice(index, 1);
+global.kocom = net.createConnection({host: serverHost, port: Number(serverPort)}, () => {
+  console.log('connected to server!');
+})
+
+global.kocom.on('data', (data) => {
+    const msgList = extractAllBetweenCharacters(data.toString('hex'), 'aa55', '0d0d');
+    msgList.forEach((msg) => {
+        const msgType = getMsgType(msg)
+        switch (MSG_TYPE[msgType as unknown as keyof typeof MSG_TYPE]) {
+            case '송신':
+                receiveMsg(msg)
+                break;
+            default:
+                console.log('알수없는패킷',msg)
         }
     })
-    let chunk: string = '';
-    let timer: NodeJS.Timeout;
-    client.on('data', function(data){
-        chunk += data.toString('hex')
-        if(timer) clearTimeout(timer);
-        timer = setTimeout(()=>{
-            broadcast(chunk, client);
-            const msgList = extractAllBetweenCharacters(chunk, 'aa55', '0d0d');
-            msgList.forEach((msg) => {
-                const msgType = getMsgType(msg)
-                switch (MSG_TYPE[msgType as unknown as keyof typeof MSG_TYPE]) {
-                    case '송신':
-                        receiveMsg(msg)
-                        break;
-                    default:
-                        console.log('알수없는패킷',msg)
-                }
-            })
-            chunk = '';
-        },500)
-    })
-});
+})
 
-server.listen(9999, function() {})
+
 
 export enum MSG_TYPE {
     '30b' = '송신',
@@ -62,16 +48,6 @@ function extractAllBetweenCharacters(inputString: string, startChar: string, end
         matches.push(match[1]);
     }
     return matches.length > 0 ? matches : [];
-}
-
-function broadcast(message: string, sender: net.Socket) {
-    console.log(clients)
-    clients.forEach((client) => {
-        if (client !== sender) {
-            console.log('send',message)
-            client.write(message, 'hex');
-        }
-    });
 }
 
 
